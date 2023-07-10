8e16b3145efc2c626bf01e382513015e85c73027
brian <liar88828@gmail.com>
2023-07-10 19:40:11 +0700

    make form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,24 @@ export default function Home ()
 {
   return (
     <main className="">
-      hello 
+      <h1 className="text-3xl font-bold text-center">
+        Product Warehouse
+      </h1>
+
+      <form action="" className='flex flex-col gap-5 max-w-xl mx-auto p-5'>
+        <input
+          type="text"
+          placeholder='Enter Product name...'
+          className='border border-gray-300 p-2 rounded-md' />
+        <input
+          type="text"
+          placeholder='Enter Product name...'
+          className='border border-gray-300 p-2 rounded-md' />
+        <button className='bg-blue-500 p-2 rounded-md text-white'>Add Product</button>
+      </form>
+
+
+
     </main>
   )
 }
