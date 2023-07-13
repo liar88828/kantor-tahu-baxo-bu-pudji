142fa436da7b813a763a0b6d1640a718053c77ea
brian <liar88828@gmail.com>
2023-07-13 12:41:08 +0700

    upload image waras

--- src/app/api/hello/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest } from 'next';
-
-export async function GET(request: NextApiRequest):Promise<Response> {
-  return new Response('Hello, Next.js!')
-}
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,70 +1,112 @@
 "use client"
 import React, { useState } from 'react';
+import FileUploadForm from '@/app/component/FileUploadForm';
 
-type TProps = {
-  value1: string
-  value2: number
-}
-
-function Products() {
-
-  const [ description, setDescription ] = useState( "" )
-  const [ selectedFile, setSelectedFile ] = useState<File | null>( null );
-  // FormEventHandler<HTMLFormElement>
-
-  const handleFileInput = ( event: React.ChangeEvent<HTMLInputElement> ) => {
-    const file = event.target.files ? event.target.files[ 0 ] : null;
-    setSelectedFile( file );
-  };
-
-  const submit = async ( event:
-      React.FormEvent<HTMLFormElement>
-      | React.ChangeEvent<HTMLInputElement>
-    // | FormEventHandler<HTMLFormElement>
-  ): Promise<any> => {
-    event.preventDefault()
-
-    const formData = new FormData()
-    if( selectedFile == null ) {
-      return null
-    }
-    formData.append( "image", selectedFile )
-    formData.append( "description", description )
-
-    const result = await fetch( '/api/images', {
-        method: "POST",
-        body: formData,
-        headers: { 'Content-Type': 'multipart/form-data' },
-      }
-    )
-    console.log( result )
-  }
-
+export default function Home() {
+  const [ salah, setSalah ] = useState<boolean>( false );
   return (
+    <main className="flex min-h-screen p-24 flex-row">
 
-    <div>
-      <div className="">
+      <FormProduct salah={ salah }/>
+      <div className="p-2">
 
-        <h1>Form Product</h1>
+        <FileUploadForm/>
       </div>
-      <div className="App">
-        <form onSubmit={ submit }>
-          <input
-            // @ts-ignore
-            filename={ selectedFile }
-            onChange={ handleFileInput }
-            type="file"
-            accept="image/*"
-          ></input>
-          <input
-            onChange={ e => setDescription( e.target.value ) }
-            type="text"
-          ></input>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    </div>
+
+    </main>
   );
 }
 
-export default Products;
+const FormProduct = ( { salah }: { salah: boolean } ) => {
+
+  return ( <>
+      <main>
+        <form className="w-full max-w-lg">
+          <div className="flex flex-wrap -mx-3 mb-6">
+
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                     htmlFor="grid-password">
+                Nama Product
+              </label>
+              <input
+                className={ `appearance-none block w-full bg-gray-100 text-gray-700 border
+               ${ !salah ? "" : "border-red-500" }
+                
+                rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white` }
+                id="grid-first-name"
+                type="text"
+                placeholder="Jane"/>
+
+              <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+            </div>
+
+            <div className="w-full md:w-1/2 px-3">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                     htmlFor="grid-last-name">
+                Last Name
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name" type="text" placeholder="Doe"/>
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                     htmlFor="grid-password">
+                Password
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-password" type="password" placeholder="******************"/>
+              <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-2">
+            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-city">
+                City
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-city" type="text" placeholder="Albuquerque"/>
+            </div>
+            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                     htmlFor="grid-state">
+                State
+              </label>
+              <div className="relative">
+                <select
+                  className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-state">
+                  <option>New Mexico</option>
+                  <option>Missouri</option>
+                  <option>Texas</option>
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-zip">
+                Zip
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-zip" type="text" placeholder="90210"/>
+            </div>
+          </div>
+        </form>
+      </main>
+
+    </>
+  )
+}
+
+
+
