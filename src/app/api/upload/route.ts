c0687f96d75308f76808c67650829e326caabb9f
brian <liar88828@gmail.com>
2023-07-24 17:41:56 +0700

    api upload image

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,19 +1,55 @@
 import fs from "fs";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST( req: Request ) {
   const formData = await req.formData();
-  console.log(formData)
 
-  const formDataEntryValues = Array.from(formData.values());
-  for (const formDataEntryValue of formDataEntryValues) {
-    if (typeof formDataEntryValue === "object" && "arrayBuffer" in formDataEntryValue) {
+  const folderName = 'public/product/';
+
+  if( !fs.existsSync( folderName ) ) {
+    console.log( 'folder create' );
+    fs.mkdirSync( folderName );
+  }
+  let dataArray = []
+  const formDataEntryValues = Array.from( formData.values() );
+  for( const formDataEntryValue of formDataEntryValues ) {
+    if( typeof formDataEntryValue === "object" && "arrayBuffer" in formDataEntryValue ) {
       const file = formDataEntryValue as unknown as Blob;
-      const buffer = Buffer.from(await file.arrayBuffer());
 
+      // console.log(formDataEntryValues[0])
+      dataArray.push( formDataEntryValues[ 0 ] )
+      const buffer = Buffer.from( await file.arrayBuffer() );
+      if( fs.existsSync( folderName + file.name ) ) {
+        console.log( 'file exists' );
+      }
+      else {
+        console.log( 'file not found!' );
+        fs.writeFileSync( folderName + file.name, buffer );
 
-      fs.writeFileSync(`public/${file.name}`, buffer);
+      }
     }
   }
-  return NextResponse.json({ success: true });
+  console.log( dataArray )
+  return NextResponse.json( { success: true } );
 }
+
+// const folderName = 'public/product/';
+// try {
+//   if( !fs.existsSync( folderName ) ) {
+//     fs.mkdirSync( folderName );
+//   }
+//   const formDataEntryValues = Array.from( formData.values() );
+//   for( const formDataEntryValue of formDataEntryValues ) {
+//     if( typeof formDataEntryValue === "object" && "arrayBuffer" in formDataEntryValue ) {
+//       const file = formDataEntryValue as unknown as Blob;
+//       const buffer = Buffer.from( await file.arrayBuffer() );
+//
+//       fs.writeFileSync( `public/product/${ file.name }`, buffer );
+//     }
+//   }
+// }
+// } catch ( err ) {
+//   console.error( err );
+// }
+// return NextResponse.json( { success: true } );
+// }
